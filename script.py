7c04446ab67c76b04364ebc9bd0950ba488e2381
dreynes <d.reynes@alumnos.upm.es>
2023-03-08 21:23:42 +0100

    fix errors in script

--- script.py
+++ script.py
@@ -1,51 +1,79 @@
 
 import xml.etree.ElementTree as ET
 import os
-from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as pyplot
+
+from wordcloud import WordCloud, STOPWORDS
 from grobid_client.grobid_client import GrobidClient
 
-#DONE
-def keywordClouds(currentPath):
+'''Function to find the links'''
+def links(myPath):
+    '''Check if the folder exists'''
+    if not(os.path.exists(myPath+'/links')):
+        os.mkdir(myPath+'/links')
 
-    '''Se configuran las stopwords para la wordcloud'''
+    arrLinks = []
+
+    for xml in os.listdir(myPath+'/xmls'):
+        tree = ET.parse(myPath+'/xmls/'+xml)
+        root = tree.getroot()
+        for element in root.findall('.//{http://www.tei-c.org/ns/1.0}p'):
+            text = element.text
+            splitText = text.split()
+            for word in splitText:
+                if 'https://' in word:
+                    if word[-1] == '.':
+                        new_word = word.replace(word[-1], '')
+                        arrLinks.append(new_word)
+                    else:
+                        arrLinks.append(word)
+                        
+    content = '\n'.join(arrLinks)
+    file = open(myPath+'/links/links.txt', 'w')
+    file.writelines(content)
+
+
+'''Function to create a wordcloud per article'''
+def keywordClouds(myPath):
+
+    '''Add words no relevants to stopwords'''
 
     stopwords = STOPWORDS
     stopwords.add('et')
     stopwords.add('al')
 
 
-    '''Se crea la carpeta en la que se guardaran los wordclouds si no existe ya'''
+    '''Check if the folder exists if not create one'''
 
-    if not(os.path.exists(currentPath+'/wordclouds')):
-        os.mkdir(currentPath+'/wordclouds')
+    if not(os.path.exists(myPath+'/wordclouds')):
+        os.mkdir(myPath+'/wordclouds')
 
 
-    '''Algoritmo para crear una wordcloud por cada xml que ha devuelto Grobid'''
+    '''Loop to create the keyWordcloud'''
 
-    for xml in os.listdir(currentPath+'/papers'):
-        tree = ET.parse(currentPath+'/papers/'+xml)
+    for xml in os.listdir(myPath+'/xmls'):
+        tree = ET.parse(myPath+'/xmls/'+xml)
         root = tree.getroot()
         element = root.find('.//{http://www.tei-c.org/ns/1.0}abstract/{http://www.tei-c.org/ns/1.0}div/{http://www.tei-c.org/ns/1.0}p')
         text = element.text
 
         wc = WordCloud(background_color = 'white', stopwords = stopwords, height = 600, width = 600)
         wc.generate(text)
-        wc.to_file(currentPath+'/wordclouds/'+xml+'_keyword_cloud.png')
+        wc.to_file(myPath+'/wordclouds/'+xml+'_keyword_cloud.png')
 
 
-#DONE
-def figuresPerArticle(currentPath):
+'''Function to count a do a plot of the figures per article'''
+def figures(myPath):
     y = []
     x = []
     i = 1
     
 
-    if not(os.path.exists(currentPath+'/figures')):
-        os.mkdir(currentPath+'/figures')
+    if not(os.path.exists(myPath+'/figures')):
+        os.mkdir(myPath+'/figures')
 
-    for xml in os.listdir(currentPath+'/papers'):
-        tree = ET.parse(currentPath+'/papers/'+xml)
+    for xml in os.listdir(myPath+'/xmls'):
+        tree = ET.parse(myPath+'/xmls/'+xml)
         root = tree.getroot()
         x.append('Paper '+ '\n' +str(i)+'')
         i += 1
@@ -56,51 +84,22 @@ def figuresPerArticle(currentPath):
 
     pyplot.bar(x, y, label = 'Number of Figures')
     pyplot.title('FIGURES PER ARTICLE')
-    pyplot.savefig(currentPath+'/figures/figsPerArticle.png')
+    pyplot.savefig(myPath+'/figures/graphFigures.png')
+
+myPath = os.getcwd()
+
+'''Check if the folder exists'''
+
+if not(os.path.exists(myPath+'/xmls')):
+    os.mkdir(myPath+'/xmls')
 
 
-
-def listOfLinks(currentPath):
-    if not(os.path.exists(currentPath+'/links')):
-        os.mkdir(currentPath+'/links')
-
-    linkList = []
-
-    for xml in os.listdir(currentPath+'/papers'):
-        tree = ET.parse(currentPath+'/papers/'+xml)
-        root = tree.getroot()
-        for element in root.findall('.//{http://www.tei-c.org/ns/1.0}p'):
-            text = element.text
-            splitText = text.split()
-            for word in splitText:
-                if 'https://' in word:
-                    if word[-1] == '.':
-                        new_word = word.replace(word[-1], '')
-                        linkList.append(new_word)
-                    else:
-                        linkList.append(word)
-                        
-    content = '\n'.join(linkList)
-    file = open(currentPath+'/links/listOfLinks.txt', 'w')
-    file.writelines(content)
-
-
-#SCRIPT
-currentPath = os.getcwd()
-
-'''Se comprueba si hay creada ya una carpeta papers en la que guardar los xmls'''
-
-if not(os.path.exists(currentPath+'/papers')):
-    os.mkdir(currentPath+'/papers')
-
-
-'''Se llama al cliente, pasandole como input el directorio en el que estan los papers(pdf)
-   y output el directorio que utilizará el script para coger los xmls que devuelve Grobid''' 
+'''Call the grobid client to process the pdf''' 
 
 client = GrobidClient(config_path='./config.json')
-client.process('processFulltextDocument', currentPath+'/pdfs', currentPath+'/papers', n=20)
+client.process('processFulltextDocument', myPath+'/pdfs', myPath+'/xmls', n=20)
 
 
-keywordClouds(currentPath)
-figuresPerArticle(currentPath)
-listOfLinks(currentPath)
+keywordClouds(myPath)
+figures(myPath)
+links(myPath)
